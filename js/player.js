61f77dc839291bacf55967bcb0b57f8eebe1173e
ADRIJA BHATTACHARJEE <65231508+momo200716@users.noreply.github.com>
2020-08-15 19:51:52 +0530

    Add files via upload

---
+++ js/player.js
@@ -0,0 +1,33 @@
+class Player{
+    constructor(){
+this.name = null
+this.distance = 0;
+this.index = 0;
+
+    }
+    getplayerCount(){
+        var countref = database.ref('playerCount')
+        countref.on('value', function(data){
+            playerCount = data.val();
+        })
+    }
+    updateData(count){
+        database.ref('/').update({playerCount : count})
+    }
+    update(){
+        var playerIndex = "players/player"+this.index
+        database.ref(playerIndex).set({
+            name:this.name, distance:this.distance
+        })
+    }
+    static getplayerinfo(){
+var playerref = database.ref("players")
+playerref.on("value",(data)=>{
+    playerinfo = data.val();
+})
+    }
+    static playerCount(){
+var playerNode = "players"
+database.ref(playerNode).set('')
+    }
+}
